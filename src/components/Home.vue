44bda7471b0f06982abe93d339bcb3088c456967
赵钊 <9350425+zhao-huaxi@user.noreply.gitee.com>
2022-04-23 13:51:02 +0800

    用户列表的开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,22 +1,147 @@
 <template>
-  <div>
+  <el-container class="home_container">
+    <!-- 头部 -->
+    <el-header>
+      <div>
+        <img src="../assets/logo.png" class="header_image" alt="" />
+        <span>电商后台管理系统</span>
+      </div>
       <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 侧边栏 -->
+    <el-container>
+      <el-aside :width="isCollapse ? '65px' : '200px'">
+        <!-- 菜单栏收起与展开 -->
+        <div class="aside_toggle" @click="toggleCollapse">|||</div>
+          <el-menu 
+          background-color="#333744" 
+          text-color="#fff" 
+          active-text-color="#409EFF" 
+          unique-opened
+          :collapse="isCollapse"
+          :collapse-transition="false"
+          router
+          :default-active="activePath"
+          >
+            <el-submenu :index="String(item.id)" v-for="item in menuList" :key="item.id">
+              <template slot="title">
+                <i :class="iconObj[item.id]"></i>
+                <span>{{item.authName}}</span>
+              </template>
+              <!-- 二级菜单 -->
+                <el-menu-item 
+                :index="'/'+ subItem.path"  
+                v-for="subItem in item.children" 
+                :key="subItem.id"
+                @click="saveNavState('/'+ subItem.path)"
+                >
+                <template slot="title">
+                  <i class="el-icon-menu"></i>
+                 <span>{{subItem.authName}}</span>
+                </template>
+                </el-menu-item>
+            </el-submenu>
+          </el-menu>
+      </el-aside>
 
-  </div>
+      <!-- 内容 -->
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-    name:'home',
-    methods:{
-        logout(){
-            window.sessionStorage.clear()
-            this.$router.push('/login')
-        }
+  name: "home",
+  data() {
+    return {
+      menuList:[],
+      iconObj:{
+        '125':'iconfont icon-user',
+        '103':'iconfont icon-tijikongjian',
+        '101':'iconfont icon-shangpin',
+        '102':'iconfont icon-danju',
+        '145':'iconfont icon-baobiao',
+      },
+      isCollapse:false, //是否折叠
+      activePath:'' //状态激活地址
     }
-}
+  },
+  created() {
+    this.getMenuList()
+    this.activePath = window.sessionStorage.getItem('activePath')
+  },
+  methods: {
+    logout() {
+      window.sessionStorage.clear();
+      this.$router.push("/login");
+    },
+    // 获取所有的菜单
+    async getMenuList(){
+      const {data:res} = await this.$http.get('menus')
+      if(res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.menuList = res.data
+      // console.log(res);
+    },
+    // 菜单展开折叠
+    toggleCollapse(){
+      this.isCollapse = !this.isCollapse 
+    },
+    // 保存连接激活状态
+    saveNavState(activePath){
+        window.sessionStorage.setItem('activePath',activePath)
+        this.activePath = activePath
+    }
+  },
+};
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.home_container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  font-size: 20px;
+  color: #fff;
+  > div {
+    display: flex;
+    align-items: center;
+  }
+}
+.el-submenu .el-menu-item{
+  min-width: 0;
+}
+.el-menu{
+  border-right:none;
+}
+.header_image {
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background-color: #eaedf1;
+}
+.iconfont{
+  margin-right: 5px;
+}
+.aside_toggle{
+  text-align: center;
+  letter-spacing: 0.2rem;
+  background: #4a5064;
+  line-height: 26px;
+  color: #fff;
+  font-size: 10px;
+  cursor: pointer;
+}
 </style>
---
+++ src/components/user/Users.vue
@@ -0,0 +1,383 @@
+<template>
+  <div>
+    <!-- 面包屑 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片视图 -->
+
+    <el-card>
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-input
+            placeholder="请输入内容"
+            v-model="queryInfo.query"
+            clearable
+            @clear="getUserList"
+          >
+            <el-button
+              slot="append"
+              @click="getUserList"
+              icon="el-icon-search"
+            ></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary" @click="addDialogVisible = true"
+            >添加用户</el-button
+          >
+        </el-col>
+      </el-row>
+
+      <el-table
+        :data="userList"
+        style="width: 100%"
+        :default-sort="{ prop: 'date', order: 'descending' }"
+        border
+      >
+        <el-table-column
+          type="index"
+          label="序号"
+          width="52px"
+        ></el-table-column>
+        <el-table-column
+          prop="username"
+          label="用户名"
+          sortable
+          width="170"
+        ></el-table-column>
+        <el-table-column
+          prop="create_time"
+          label="创建时间"
+          sortable
+          width="170"
+        ></el-table-column>
+        <el-table-column
+          prop="email"
+          label="email"
+          width="170"
+        ></el-table-column>
+        <el-table-column
+          prop="mobile"
+          label="电话"
+          width="170"
+        ></el-table-column>
+        <el-table-column prop="mg_state" label="状态" width="170">
+          <template slot-scope="scope">
+            <el-switch
+              v-model="scope.row.mg_state"
+              @change="userStateChanged(scope.row)"
+            ></el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="role_name"
+          label="角色"
+          width="170"
+        ></el-table-column>
+        <el-table-column label="操作" width="180">
+          <template slot-scope="scope">
+            <el-button
+              type="primary"
+              icon="el-icon-edit"
+              size="mini"
+              @click="showEditDialogVisible(scope.row.id)"
+            ></el-button>
+            <el-button
+              type="danger"
+              icon="el-icon-delete"
+              size="mini"
+              @click="deleteUser(scope.row.id)"
+            ></el-button>
+
+            <el-tooltip
+              class="item"
+              effect="dark"
+              content="分配角色"
+              placement="top"
+            >
+              <el-button
+                type="warning"
+                icon="el-icon-setting"
+                size="mini"
+              ></el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <div class="block">
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="queryInfo.pagenum"
+          :page-sizes="[1, 2, 5, 10]"
+          :page-size="queryInfo.pagesize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total"
+        >
+        </el-pagination>
+      </div>
+    </el-card>
+
+    <!-- 添加用户 提示框 -->
+    <el-dialog
+      title="添加用户"
+      :visible.sync="addDialogVisible"
+      width="40%"
+      @close="addFromClose"
+    >
+      <!-- 内容主体区域 -->
+      <el-form
+        :model="addForm"
+        :rules="addFormRules"
+        ref="addRuleFormRef"
+        label-width="80px"
+      >
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="addForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="addForm.password"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="addForm.email"></el-input>
+        </el-form-item>
+        <el-form-item label="电话号码" prop="mobile">
+          <el-input v-model="addForm.mobile"></el-input>
+        </el-form-item>
+      </el-form>
+      <!-- 底部区域 -->
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addUser">确 定</el-button>
+      </span>
+    </el-dialog>
+
+    <el-dialog
+  title="修改用户信息"
+  :visible.sync="editDialogVisible"
+  width="40%"
+  @close="editDialogClose"
+  >
+ <el-form :model="editForm" :rules="editFormRules" ref="editFormRef" label-width="80px">
+  <el-form-item label="用户名">
+    <el-input v-model="editForm.username" disabled></el-input>
+  </el-form-item>
+   <el-form-item label="邮箱" prop="email">
+    <el-input v-model="editForm.email"></el-input>
+  </el-form-item>
+   <el-form-item label="电话号码" prop="mobile">
+    <el-input v-model="editForm.mobile"></el-input>
+  </el-form-item>
+ </el-form>
+  <span slot="footer" class="dialog-footer">
+    <el-button @click="editDialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="editUserInfo">确 定</el-button>
+  </span>
+</el-dialog>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    var checkEmail = (rule, value, cb) => {
+      const regEmail = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/;
+
+      if (regEmail.test(value)) {
+        return cb();
+      }
+
+      cb(new Error("请输入合法的邮箱地址！"));
+    };
+
+    var checkMobile = (rule, value, cb) => {
+      const regMobile =
+        /^(0|86|17951)?(13[0-9]|15[0123456789]|17[678]|18[0-9]|14[57])[0-9]{8}$/;
+      if (regMobile.test(value)) {
+        return cb();
+      }
+
+      cb(new Error("请输入合法的手机号码！"));
+    };
+    return {
+      queryInfo: {
+        query: "",
+        pagenum: 1,
+        pagesize: 10,
+      },
+      total: 0,
+      userList: [],
+      addDialogVisible: false,
+      addForm: {
+        username:'',
+        password:'',
+        email:'',
+        mobile:''
+      },
+      addFormRules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          {
+            min: 3,
+            max: 10,
+            message: "长度在 3 到 10 个字符",
+            trigger: "blur",
+          },
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          {
+            min: 6,
+            max: 15,
+            message: "长度在 6 到 15 个字符",
+            trigger: "blur",
+          },
+        ],
+        email: [
+          { required: true, message: "请输入邮箱", trigger: "blur" },
+          { validator: checkEmail, trigger: "blur" },
+        ],
+        mobile: [
+          { required: true, message: "请输入电话", trigger: "blur" },
+          { validator: checkMobile, trigger: "blur" },
+        ],
+      },
+      editDialogVisible:false,
+      editForm:{},
+      editFormRules:{
+        email: [
+          { required: true, message: "请输入邮箱", trigger: "blur" },
+          { validator: checkEmail, trigger: "blur" },
+        ],
+        mobile: [
+          { required: true, message: "请输入电话", trigger: "blur" },
+          { validator: checkMobile, trigger: "blur" },
+        ],
+      }
+    };
+  },
+  created() {
+    this.getUserList();
+  },
+  methods: {
+    async getUserList() {
+      //请求数据
+      const { data: res } = await this.$http.get("users", {
+        params: this.queryInfo,
+      });
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg);
+      this.total = res.data.total;
+      this.userList = res.data.users;
+    },
+
+    handleSizeChange(newSize) {
+      this.queryInfo.pagesize = newSize;
+      this.getUserList();
+    },
+
+    handleCurrentChange(newNum) { //监听页码值得改变
+      this.queryInfo.pagenum = newNum;
+      this.getUserList();
+    },
+
+    async userStateChanged(userinfo) {
+      // 监听swich开关的改变
+      const { data: res } = await this.$http.put(
+        `users/${userinfo.id}/state/${userinfo.mg_state}`
+      );
+      if (res.meta.status !== 200) {
+        userinfo.mg_state = !userinfo.mg_state;
+        return this.$message.error("更新用户状态失败！");
+      }
+      this.$message.success("更新用户状态成功！");
+    },
+
+    addFromClose() {
+      //添加用户提示框关闭的表单重置事件
+      this.$refs.addRuleFormRef.resetFields();
+    },
+
+    addUser() {//添加用户的请求
+      this.$refs.addRuleFormRef.validate(async valid =>{
+        if(!valid) return
+         const {data:res} =  await this.$http.post('users',this.addForm)
+         console.log(res);
+        if (res.meta.status != 201) return this.$message.error('添加失败')
+        this.$message.success('添加用户成功!')
+        this.addDialogVisible = false
+        this.getUserList()
+      })  
+    },
+    async showEditDialogVisible(id){
+      const {data:res}  = await this.$http.get('users/'+id)
+      if(res.meta.status!=200) return this.$message.error('获取用户信息失败!')
+      this.editForm = res.data
+      this.editDialogVisible = true
+    },
+    editDialogClose(){
+      this.$refs.editFormRef.resetFields()
+    },
+    editUserInfo(){
+      this.$refs.editFormRef.validate(async valid =>{
+        if(!valid) return
+        const {data:res} =  await this.$http.put('users/'+this.editForm.id,
+          {
+            email:this.editForm.email,
+            mobile:this.editForm.mobile
+          }
+        )
+        if(res.meta.status != 200) return this.$message.error('更新用户信息失败!')
+        this.editDialogVisible = false
+        this.getUserList()
+        this.$message.success('更新用户信息成功!')
+
+      })
+    },
+    deleteUser(id) {
+        this.$confirm('此操作将永久删除该用户, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(async () => {
+
+          const {data:res} = await this.$http.delete('users/' +id)
+          if(res.meta.status != 200) return this.$message.error('删除失败!')
+          this.$message({
+            type: 'success',
+            message: '删除成功!'
+          });
+
+          this.getUserList()
+          
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          });          
+        });
+      }
+    }
+};
+</script>
+
+<style lang="less" scoped>
+.el-breadcrumb {
+  margin-bottom: 15px;
+  font-size: 12px;
+}
+.el-card {
+  box-shadow: 0 1px 1px rgba(0, 0, 0blue, 0.15) !important;
+}
+.el-table {
+  margin-top: 15px;
+}
+.el-pagination {
+  margin-top: 15px;
+}
+</style>
